3d0773430a33233e0483499bafd294e8eb9abd12
LJRollerII <ljcoolkid@optonline.net>
2022-11-13 12:57:27 -0500

    Started initial code.

---
+++ scripts/planner.py
@@ -0,0 +1,11 @@
+import csv
+
+def read_tasks(filename):
+    tasks = {}
+    for row in csv.reader(open(filename)):
+        number = int(row[0])
+        title = row[1]
+        duration =float(row[2])
+        prerequisites = set(map(int, row[3].split()))
+        tasks[number] = (title, duration, prerequisites)
+    return tasks
